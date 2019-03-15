431f10fb235a67bccb7ce801452b68816734ae1f
Jamie Thomson <jamthomweb@gmail.com>
2019-03-15 12:38:05 +0100

    google analytics update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './fonts/index.css'
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-133839923-1');
-ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.getElementById('app'));
